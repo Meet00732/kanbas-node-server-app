58a8cd54a5a28892abbb8f4188cc862ef55c6661
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2023-12-03 14:45:36 -0500

    Assignment6 changes-2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import session from "express-session";
 import "dotenv/config";
 import Hello from './hello.js';
-import Lab5 from './Lab5.js';
+import Lab5 from './lab5_1.js';
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from './modules/routes.js';
