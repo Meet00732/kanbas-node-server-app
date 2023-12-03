e6ddc47925bf61e9bcf5839b918c0453e2f91d1a
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2023-12-03 13:59:55 -0500

    Assignment6

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 import express from 'express';
+import session from "express-session";
 import "dotenv/config";
 import Hello from './hello.js';
-import Lab5 from './lab5.js';
+import Lab5 from './Lab5.js';
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from './modules/routes.js';
+import UserRoutes from "./users/routes.js";
 import AssignmentRoutes from './assignments/routes.js';
 
-const app = express();
-// app.use(cors({credentials: true,
-//     origin: process.env.FRONTEND_URL}));
-app.use(cors());
-app.use(express.json());
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
+const app = express();
+app.use(cors({credentials: true,
+    origin: process.env.FRONTEND_URL}));
+    
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+    };
+    if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+    }
+    app.use(session(sessionOptions));      
+      
+// app.use(cors());
+app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
